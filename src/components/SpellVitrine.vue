262ab02d909c004601102229ad354afa0d826e31
anivire <anivire.work@gmail.com>
2023-03-16 14:42:38 +0300

    ✨ polish: spheres fix

--- src/components/SpellVitrine.vue
+++ src/components/SpellVitrine.vue
@@ -8,15 +8,15 @@
     <div class="grid grid-cols-3 gap-3 mb-5" v-show="isShow">
         <div v-for="i in 10">  
             <div :class="(spellData[i].Id == 10)?'from-sky-500/25 via-fuchsia-500/10 to-amber-500/5'
-                 :(spellData[i].Combination[1] == 'exort')?'from-amber-500/10'
+                 :(spellData[i].Combination[1] == 'exort')?'from-amber-500/10'  
                  :(spellData[i].Combination[1] == 'quas')?'from-sky-500/10'
                  :'from-fuchsia-500/10'"
                   class="flex flex-row justify-items-center bg-gradient-to-r justify-center spell p-2 rounded-md shadow-md">
                 <!-- <SpellIcon :spellId="spellData[i].Id"></SpellIcon> -->
                 <img :src="spellData[i].Img + '.png'" class="overflow-hidden rounded-lg">
-                <div class="flex flex-col ml-4 justify-center">
+                <div class="flex flex-col ml-4 justify-center ">
                     <p :class="(spellData[i].Id == 10)?'bg-gradient-to-r from-sky-500 via-fuchsia-500 to-amber-500 bg-clip-text text-transparent':(spellData[i].Combination[1] == 'exort')?'text-amber-500':(spellData[i].Combination[1] == 'quas')?'text-sky-500':'text-fuchsia-500'">{{spellData[i].Title}}</p>
-                    <div class="flex flex-row ">
+                    <div class="flex flex-row justify-center">
                         <SphereIcon :sphereTitle="spellData[i].Combination[0]"></SphereIcon>
                         <SphereIcon :sphereTitle="spellData[i].Combination[1]"></SphereIcon>
                         <SphereIcon :sphereTitle="spellData[i].Combination[2]"></SphereIcon>
