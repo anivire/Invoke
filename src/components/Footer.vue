893cb8368ff37f62d7c920f230a1053396770147
anivire <anivire.work@gmail.com>
2023-03-16 19:02:10 +0300

    🐛 bugfix: asset fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
     <div class=" bg-zinc-800/50 w-full inset-x-0 bottom-0 p-5 mt-5">
         <div class="flex flex-row justify-center">
             <p class="text-sm">Feedback and other on</p>
-            <img class="w-4 h-4 ml-2 mr-1" src="github-mark-white.svg">
+            <img class="w-4 h-4 ml-2 mr-1" src="../assets/github-mark-white.svg">
             <a class="underline text-sm" href="https://github.com/anivire/invoke/issues/new">GitHub</a>
             <p class="text-sm">.</p>
         </div>
